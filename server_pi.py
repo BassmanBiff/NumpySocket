f5108b8144cea0a0104222c64aa3c7c0ad587090
Dave Matthews <dmatthews@distantfocus.com>
2019-03-05 19:51:23 -0600

    f

--- server_pi.py
+++ server_pi.py
@@ -9,12 +9,11 @@ npSocket.start(9999)
 camera_port = 0
 with PiCamera(camera_port, sensor_mode=2) as camera:
     with PiBayerArray(camera, output_dims=2) as stream:
-        camera.capture(
-            stream,
-            format='jpeg',  # Necessary for bayer=True
-            bayer=True,     # Include Bayer data in metadata
-            thumbnail=None)
-        while True:
+        for frame in camera.capture_continuous(
+                         stream,
+                         format='jpeg',  # Necessary for bayer=True
+                         bayer=True,     # Include Bayer data in metadata
+                         thumbnail=None):
             npSocket.send_array(stream.array)
 
 # When everything done, release the video capture object
